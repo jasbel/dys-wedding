d7758568ce837f03aecc018b48e1034a37bcd3ba
Asbel Jhonatan Apaza Romero <asbel.elmec@gmail.com>
2024-01-19 22:22:35 -0400

    feat: change data

---
+++ resource/components/sections/history/item-history.tsx
@@ -0,0 +1,22 @@
+import { component$ } from "@builder.io/qwik";
+import styles from "./history.module.scss";
+
+interface IItemHistoryProps {
+  title?: string;
+  urlBg?: string;
+  description?: string;
+}
+
+export default component$(({ title = "", description = "", urlBg }: IItemHistoryProps) => {
+  return (
+    <div class={styles.item__content} style={`background-image: url(${urlBg})`}>
+      {/* <div class={styles.item__bordered}></div> */}
+      <div class={styles.item}>
+        <h2 class={styles.item__title}>{title}</h2>
+        <div class={styles.item__descriptionWrap} >
+          <p class={styles.item__description}>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+});
---
+++ src/components/ButtonTest.jsx
@@ -0,0 +1,10 @@
+"use client"
+export const ButtonTest = () => {
+  const url = '';
+  return (
+    <>
+      <button>asdjflksadlkf</button>
+      <span>asfdsa</span>
+    </>
+  )
+}
