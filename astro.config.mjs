aee960605035325fe8bfc5e2cbfba4e089239c84
asbel.dev <aapaza@develoop.net>
2026-05-11 20:24:30 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+          loadPaths: ['.'],
+        },
+      },
+    },
+  },
 });
